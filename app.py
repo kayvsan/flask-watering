41a02c25d3d6560d49ccb29a9e4c80460aebda06
rdi-dev24 <kayyisn.chan@gmail.com>
2025-08-16 11:21:55 +0700

    remove debug mode from Flask app run configuration

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def run_app():
         )
     
     # Start Flask
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     run_app()
